12cabce8f758066868bc358744ddd80659c80165
mrwho95 <dnkh903@gmail.com>
2020-05-29 13:28:27 +0800

    build Hero and products vue, apply components and section to home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <Navbar></Navbar>
+    <Hero />
+    <Products />
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <button class="btn btn-primary">Click</button>-->
@@ -9,11 +12,14 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
+import Hero from "@/components/Hero.vue";
+import Products from "@/sections/Products.vue";
 
 export default {
-  name: "Home"
-  // components: {
-  //   HelloWorld
-  // }
+  name: "Home",
+  components: {
+    Hero,
+    Products
+  }
 };
 </script>
