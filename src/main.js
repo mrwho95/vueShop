233784659f8a359513bb07c2cbd67f20021d1dd5
mrwho95 <dnkh903@gmail.com>
2020-05-29 09:19:13 +0800

    build navbar from bootstrap, apply vue router-link

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import "./assets/app.scss";
 
 window.$ = window.jQuery = jQuery;
 
+Vue.component("Navbar", require("./components/NavBar.vue").default);
+
 Vue.config.productionTip = false;
 
 new Vue({
